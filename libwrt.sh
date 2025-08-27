6003d34477a9365be215f1626c05ebe7c8e52518
fengshutao <69944484+fengshutao@users.noreply.github.com>
2025-08-27 08:02:26 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -6,9 +6,9 @@ sed -i 's/0x1000000/0x800000/g' ./target/linux/qualcommax/files/arch/arm64/boot/
 
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 # 移除 openwrt feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+# rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
 # 移除 openwrt feeds 过时的luci版本
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+# rm -rf feeds/luci/applications/luci-app-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
